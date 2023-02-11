d5c3cb8922c9d4e3a19d900d673198a0650b6939
amit neuhaus <amit@beehero.io>
2023-02-11 12:46:30 +0200

    pattern counting using hadoop counter

--- src/main/java/MapperReducer_PatternIndexer.java
+++ src/main/java/MapperReducer_PatternIndexer.java
@@ -1,20 +1,20 @@
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Partitioner;
-import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.*;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.util.Iterator;
 
 
 public class MapperReducer_PatternIndexer {
-
     public static class Mapper_PatternIndexer extends Mapper<LongWritable, Text, Text, Text> {
         @Override
         public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
@@ -32,6 +32,8 @@ public class MapperReducer_PatternIndexer {
                 Text pattern = it.next();
                 context.write(pattern, new LongWritable(count));
                 count = count + 1;
+                Counter counter = context.getCounter("pattern-counter-group", "pattern-counter");
+                counter.increment(1);
             }
         }
 
@@ -45,6 +47,17 @@ public class MapperReducer_PatternIndexer {
         }
     }
 
+
+
+    private static void writeNumberOfPatterns(Configuration conf, Job job) throws IOException {
+        Path path = new Path("patterns-amount.txt");
+        FileSystem fs = FileSystem.get(conf);
+        OutputStream os = fs.create(path);
+        Writer writer = new OutputStreamWriter(os);
+        Counter counter = job.getCounters().findCounter("pattern-counter-group", "pattern-counter");
+        writer.write(counter.getValue() + "\n");
+        writer.close();
+    }
     public static void main(String[] args) throws Exception {
         try{
             Configuration conf = new Configuration();
@@ -60,7 +73,16 @@ public class MapperReducer_PatternIndexer {
 //        job.setInputFormatClass(SequenceFileAsTextInputFormat.class);
             FileInputFormat.addInputPath(job, new Path(args[0]));
             FileOutputFormat.setOutputPath(job, new Path(args[1]));
-            System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+            boolean success = job.waitForCompletion(true);
+
+            if (success) {
+                long counterValue = job.getCounters().findCounter("pattern-counter-group", "pattern-counter").getValue();
+                writeNumberOfPatterns(conf,job);
+                System.out.println("Counter value: " + counterValue);
+            }
+
+            System.exit(success ? 0 : 1);
         }catch(Exception e){
             System.out.println(e);
         }
